499763a07b2f2a0f6fa64effa81c6241ab5ddee9
Johannes Pirkl <johannespirkl@gmx.de>
2017-07-19 12:23:28 +0200

    Fix import.

--- src/main/scala/yaml_snippets/Snippet.scala
+++ src/main/scala/yaml_snippets/Snippet.scala
@@ -1,5 +1,7 @@
 package yaml_snippets
 
+import yaml_snippets.string_templates.StringTemplate
+
 case class Snippet(keywords:Seq[String], text:String){
   lazy val template = StringTemplate.parse(text)
 }
