9e8e6959214ec0f42a69d9722cfa748f73d8f8ee
Johannes Pirkl <johannespirkl@gmx.de>
2017-07-21 02:51:10 +0200

    Remove unused exception.

--- src/main/scala/cannery/string_templates/package.scala
+++ src/main/scala/cannery/string_templates/package.scala
@@ -3,11 +3,4 @@ import scala.annotation.tailrec
 
 package object string_templates {
   type Bindings = String => Any
-
-  class MissingVariableException(variable:String) extends RuntimeException(s"Missing binding for variable in string template: $variable.")
-
-
-
-
-
 }
